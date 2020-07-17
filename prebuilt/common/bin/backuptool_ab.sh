15f99be7ef1aa60ee5d9791ee4f5fc01549b1f67
althafvly <althafvly@gmail.com>
2020-07-17 23:19:57 +0530

    Support backuptool for malluos

--- prebuilt/common/bin/backuptool_ab.sh
+++ prebuilt/common/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=10.0
+export V=10
 
 export ADDOND_VERSION=2
 
@@ -50,7 +50,7 @@ if [ ! -r /system/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" /system/build.prop; then
+if ! grep -q "^ro.malluos.version.code=$V.*" /system/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi
