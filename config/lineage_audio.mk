a08a5a3bca213e54a80b45f91936c6c2904fa76e
althafvly <althafvly@gmail.com>
2020-08-15 14:25:19 +0530

    Add our special malluos notification ringtone
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- config/lineage_audio.mk
+++ config/lineage_audio.mk
@@ -20,7 +20,8 @@ PRODUCT_COPY_FILES += \
     $(NOTIFICATION_PATH)/Naughty.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Naughty.ogg \
     $(NOTIFICATION_PATH)/Pong.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Pong.ogg \
     $(NOTIFICATION_PATH)/Rang.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Rang.ogg \
-    $(NOTIFICATION_PATH)/Stone.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Stone.ogg
+    $(NOTIFICATION_PATH)/Stone.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Stone.ogg \
+		$(NOTIFICATION_PATH)/moylali.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/moylali.ogg
 
 # Ringtones
 PRODUCT_COPY_FILES += \
